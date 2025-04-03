7a011da867f3354ef11cd8e2565f469757eb7147
Ana Camila Almiron <almironanacamila@gmail.com>
2025-04-03 15:51:43 -0300

    correccion de App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import Card from "./Card";
 import "../styles/Cards.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../redux/store";
 
 const App = () => {
-  //const [productos, setProductos] = useState([]); // asegura que es un array
-  const productos = useSelector((store) => store.productos.productos); //linea 18 store.js
-  console.log(productos);
+  const productos = useSelector((store) => store.productos.productos);
   const dispatch = useDispatch();
+
   useEffect(() => {
-    // npx json-server src/data/db.json -p 5001
-    dispatch(getProducts()); //despachar funcion getproducts de store
-  }, []);
+    dispatch(getProducts());
+  }, [dispatch]);
   return (
     <div>
       <div className="card-container">
-        {productos.length > 0
-          ? productos.map((producto) => (
-              <Card key={producto.id} producto={producto} />
-            ))
-          : console.log("no se encontraron los resultados")}
+        {productos.length > 0 ? (
+          productos.map((producto) => (
+            <Card key={producto.id} producto={producto} />
+          ))
+        ) : (
+          <p>No se encontraron resultados</p>
+        )}
       </div>
     </div>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 
 const initialState = { productos: [], numeroPagina: 1 };
 
